dc8008580b22ee9820ad5193a49327bd184139a5
stamart <stamart@gmail.com>
2016-02-06 18:53:12 +0100

    Funkcja Main

--- src/Aplikacja3/Test.java
+++ src/Aplikacja3/Test.java
@@ -4,4 +4,7 @@ package Aplikacja3;
  * Created by staszek on 2016-02-06.
  */
 public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
